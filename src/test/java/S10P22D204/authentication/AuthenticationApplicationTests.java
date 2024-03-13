358abb73c6e0990fdf0f24685c7dba5d29f3a483
신창엽 <schy2504@naver.com>
2024-03-13 09:48:17 +0900

    Feat #1: Basic setting for authentication server
    - started spring initializr

---
+++ src/test/java/S10P22D204/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package S10P22D204.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
