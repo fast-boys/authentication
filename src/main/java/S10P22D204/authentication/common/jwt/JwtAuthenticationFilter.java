c544e400f7026a69c21860d1a7b436ac5e443938
신창엽 <schy2504@naver.com>
2024-03-28 19:41:38 +0900

    Deploy : Jwt refreshing cookie fix

--- src/main/java/S10P22D204/authentication/common/jwt/JwtAuthenticationFilter.java
+++ src/main/java/S10P22D204/authentication/common/jwt/JwtAuthenticationFilter.java
@@ -1,37 +1,35 @@
 package S10P22D204.authentication.common.jwt;
 
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.lang.NonNull;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Component
 @Order(-1)
-@RequiredArgsConstructor
-public class JwtAuthenticationFilter implements WebFilter {
+public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
 
-    private final JwtManager jwtManager;
+    @Autowired
+    private JwtManager jwtManager;
 
-    private static final List<String> PERMIT_URL_LIST = List.of(
-            /* authentication server */
-            "/auth/.*",
-            /* search server */
-            "/search/.*"
+    private static final List<Pattern> permitUrlPatterns = List.of(
+            Pattern.compile("/auth/.*"),
+            Pattern.compile("/search/.*")
     );
 
     @Override
-    public Mono<Void> filter(@NonNull ServerWebExchange exchange, @NonNull WebFilterChain chain) {
-        String path = exchange.getRequest().getPath().value();
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
 
-        boolean isPermitted = PERMIT_URL_LIST.stream().anyMatch(path::matches);
+        boolean isPermitted = permitUrlPatterns.stream().anyMatch(pattern -> pattern.matcher(path).matches());
         if (isPermitted) {
             return chain.filter(exchange);
         }
@@ -39,23 +37,20 @@ public class JwtAuthenticationFilter implements WebFilter {
         return jwtManager.checkAccessToken(exchange)
                 .flatMap(internalId -> {
                     if ("null".equals(internalId)) {
-                        System.out.println("FAILED");
                         exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
                         return exchange.getResponse().setComplete();
                     } else {
-                        System.out.println("SUCCESS");
-
-                        ServerHttpRequest.Builder requestBuilder = exchange.getRequest().mutate();
-
-                        requestBuilder.header(jwtManager.INTERNAL_ID_HEADER, internalId);
-                        requestBuilder.header(jwtManager.SECRET_KEY_HEADER, "fastand6");
-
-                        ServerWebExchange mutatedExchange = exchange.mutate()
-                                .request(requestBuilder.build())
+                        exchange.getRequest().mutate()
+                                .header(jwtManager.INTERNAL_ID_HEADER, internalId)
+                                .header(jwtManager.SECRET_KEY_HEADER, "fastand6")
                                 .build();
-
-                        return chain.filter(mutatedExchange);
+                        return chain.filter(exchange);
                     }
                 });
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
     }
 }
